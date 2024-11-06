5f838eebc8502bedc35f5692012679e997689523
asfalfa <glkin9@gmail.com>
2024-11-07 00:35:40 +0100

    feat: implement sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,28 @@
 import { defineConfig } from "astro/config";
 import vercelServerless from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
-
-import preact from "@astrojs/preact";
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercelServerless(),
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), icon(), preact()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    icon(),
+    sanity({
+      projectId: "z0aukxbh",
+      dataset: "production",
+      useCdn: false, // for static builds
+      studioBasePath: "/studio",
+    }),
+    react(),
+  ],
 });
