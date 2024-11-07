213eaa72c5e8172a5e9254071c2bab9f6a0fa1f3
asfalfa <glkin9@gmail.com>
2024-11-07 12:53:47 +0100

    sanity: add post filter by category

--- src/sanity/queries/getPosts.ts
+++ src/sanity/queries/getPosts.ts
@@ -1,12 +1,28 @@
 import type { SanityDocument } from "@sanity/client";
 import { sanityClient } from "sanity:client";
 
-export default async function getPosts() {
+export default async function getPosts(category?: string) {
+  console.log("Fetching posts with category:", category);
   const POSTS_QUERY = `*[
-    _type == "post"
-    && defined(slug.current)
-  ]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt, mainImage}`;
+  _type == "post" 
+  ${category ? `&& references(*[_type == "category" && title ==  "${category}"]._id)` : ""}]
+  {
+    title,
+    slug,
+    author->{
+      name
+    },
+    mainImage{
+      asset->{
+        url
+      },
+      alt
+    },
+    publishedAt,
+    body
+  }`;
 
   const posts = await sanityClient.fetch<SanityDocument[]>(POSTS_QUERY);
+  console.log("Retrieved posts:", posts);
   return posts;
 }
