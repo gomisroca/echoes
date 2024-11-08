e66d1306bbf352dc0b3f3f7e121b20ef348993d2
asfalfa <glkin9@gmail.com>
2024-11-08 10:09:40 +0100

    style: add prettier

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -45,23 +45,23 @@ const Search = () => {
   };
 
   return (
-    <div className="flex flex-col gap-4 w-full">
+    <div className="flex w-full flex-col gap-4">
       <input
         type="text"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Search..."
-        className="bg-zinc-100 dark:bg-zinc-900 p-4 ring-2 ring-emerald-200 rounded-xl w-1/3 mx-auto"
+        className="mx-auto w-1/3 rounded-xl bg-zinc-100 p-4 ring-2 ring-emerald-200 dark:bg-zinc-900"
       />
       <button
         onClick={() => handleSearch()}
         disabled={formState.isSubmitting}
-        className="w-1/3 mx-auto ring-2 ring-emerald-300 rounded-xl p-4 duration-200 hover:ring-emerald-400 hover:scale-105 hover:shadow-lg hover:shadow-emerald-600/20 active:scale-110 active:shadow-emerald-600/40 active:duration-100"
+        className="mx-auto w-1/3 rounded-xl p-4 ring-2 ring-emerald-300 duration-200 hover:scale-105 hover:shadow-lg hover:shadow-emerald-600/20 hover:ring-emerald-400 active:scale-110 active:shadow-emerald-600/40 active:duration-100"
       >
         Search
       </button>
       {<p>{formState.message}</p>}
-      <div className="flex flex-col gap-4 w-full">
+      <div className="flex w-full flex-col gap-4">
         {results.map((post) => (
           <div key={post._id}>
             <a
@@ -70,16 +70,16 @@ const Search = () => {
                   ? `/one-shots/${post.slug.current}`
                   : `/series/${post.series.slug.current}/${post.slug.current}`
               }
-              className="group relative rounded-xl ring-2 ring-emerald-300 flex flex-row justify-center items-center mx-auto duration-200 hover:ring-emerald-400 hover:scale-105 hover:shadow-lg hover:shadow-emerald-600/20 active:scale-110 active:shadow-emerald-600/40 active:duration-100"
+              className="group relative mx-auto flex flex-row items-center justify-center rounded-xl ring-2 ring-emerald-300 duration-200 hover:scale-105 hover:shadow-lg hover:shadow-emerald-600/20 hover:ring-emerald-400 active:scale-110 active:shadow-emerald-600/40 active:duration-100"
             >
               <img
                 src={post.mainImage.asset.url}
                 alt={post.title}
                 width={500}
                 height={500}
-                className="absolute top-0 left-0 w-full h-full object-cover object-center rounded-xl"
+                className="absolute left-0 top-0 h-full w-full rounded-xl object-cover object-center"
               />
-              <div className="p-4 flex flex-col justify-start items-start h-full flex-1 gap-4 z-10 m-4 rounded-xl bg-zinc-100/60 dark:bg-zinc-900/60 backdrop-blur-sm group-hover:bg-zinc-100 dark:group-hover:bg-zinc-900 duration-200">
+              <div className="z-10 m-4 flex h-full flex-1 flex-col items-start justify-start gap-4 rounded-xl bg-zinc-100/60 p-4 backdrop-blur-sm duration-200 group-hover:bg-zinc-100 dark:bg-zinc-900/60 dark:group-hover:bg-zinc-900">
                 <h2 className="text-2xl font-semibold">{post.title}</h2>
                 <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
               </div>
