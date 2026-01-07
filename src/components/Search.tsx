b8ded223229a5e7a28ff7af00c3ffc6ea9d072b8
Adrià Gomis <gomisroca@gmail.com>
2026-01-07 22:59:03 +0100

    remove unused import

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,5 @@
 import { type SanityDocument } from "@sanity/client";
 import { useState } from "react";
-import { sanityClient } from "sanity:client";
-import SanityImage from "./SanityImage.astro";
 import { searchPosts } from "@/sanity/queries/posts";
 
 interface FormState {
@@ -39,7 +37,7 @@ const Search = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Search..."
-        className="mx-auto w-full rounded-xl bg-zinc-100 p-4 ring-2 ring-emerald-200 transition duration-200 ease-in-out focus-visible:scale-105 focus-visible:shadow-lg focus-visible:shadow-emerald-600/20 focus-visible:outline-solid focus-visible:outline-offset-2 focus-visible:outline-emerald-300/25 lg:w-1/3 dark:bg-zinc-900"
+        className="mx-auto w-full rounded-xl bg-zinc-100 p-4 ring-2 ring-emerald-200 transition duration-200 ease-in-out focus-visible:scale-105 focus-visible:shadow-lg focus-visible:shadow-emerald-600/20 focus-visible:outline-offset-2 focus-visible:outline-emerald-300/25 focus-visible:outline-solid lg:w-1/3 dark:bg-zinc-900"
       />
       <button
         onClick={() => handleSearch()}
@@ -67,7 +65,7 @@ const Search = () => {
                 alt={post.title}
                 width={500}
                 height={500}
-                className="absolute left-0 top-0 h-full w-full rounded-xl object-cover object-center"
+                className="absolute top-0 left-0 h-full w-full rounded-xl object-cover object-center"
               />
               <div className="z-10 m-2 flex h-full flex-1 flex-col items-start justify-start gap-2 rounded-xl bg-zinc-100/60 p-4 backdrop-blur-xs duration-200 ease-in-out group-hover:bg-zinc-100 dark:bg-zinc-900/60 dark:group-hover:bg-zinc-900">
                 <h2>{post.title}</h2>
